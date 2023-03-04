102518969194db9227275662c988575f3a694d0f
gstmoniz <gustavo.moniz.ribeiro@gmail.com>
2023-03-03 22:42:10 -0300

    030323T22h41

--- ambiente/desenvolvimento/config_script.sh
+++ ambiente/desenvolvimento/config_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 sudo service docker start
-sudo docker pull gstmoniz/app-node:1.4
 sudo docker run -p 8080:6000 -d gstmoniz/app-node:1.4
--- infraestrutura/main.tf
+++ infraestrutura/main.tf
@@ -13,6 +13,11 @@ provider "aws" {
   region  = var.regiao_aws
 }
 
+resource "aws_key_pair" "ssh-key" {
+  key_name = var.chave
+  public_key = file("${var.chave}.pub")
+}
+
 resource "aws_launch_template" "serverAutoScale" {
   image_id = "ami-0a8bcde34acb334ab"
   # SUSE Linux Enterprise Server 15 SP4 64-bit
@@ -44,10 +49,46 @@ resource "aws_autoscaling_group" "scaleGroup" {
     id = aws_launch_template.serverAutoScale.id
     version = "$Latest"
   }
+  target_group_arns = [ aws_lb_target_group.ELB_target.arn ]
+}
+# target_group_arns -> set of aws_alb_target_group arns for use with ALB
+# defines that the resource is the target of the other
+
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = "${var.regiao_aws}a"
 }
 
-resource "aws_key_pair" "ssh-key" {
-  key_name = var.chave
-  public_key = file("${var.chave}.pub")
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = "${var.regiao_aws}b"
 }
 
+resource "aws_default_vpc" "default" {
+}
+
+# LOAD BALANCER
+
+resource "aws_lb" "loadBalancer" {
+  internal = false
+  load_balancer_type = "application"
+  subnets = [ aws_default_subnet.subnet_1.id,aws_default_subnet.subnet_2.id ]
+  tags = {
+    Name = "${var.nome_instancia}-ELB"
+  }
+}
+
+resource "aws_lb_target_group" "ELB_target" {
+  name = "targetASGroup"
+  port = "8080"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_lb_listener" "ELB_listener" {
+  load_balancer_arn = aws_lb.loadBalancer.arn
+  port = "8080"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.ELB_target.arn
+  }
+}
